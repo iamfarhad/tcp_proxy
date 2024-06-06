db27349e52352aecd4ce7e11206aa8d8b053fd93
iamfarhad <farhad.pd@gmail.com>
2024-06-06 21:05:14 +0330

    change

--- main.go
+++ main.go
@@ -4,104 +4,78 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net"
 )
 
 const (
 	ClientMode = "client"
 	ServerMode = "server"
-	BufferSize = 1024
+	BufferSize = 32 * 1024 // 32KB buffer size
 )
 
-func handleClient(client net.Conn, serverAddr string) {
-	defer client.Close()
-
-	serverConn, err := net.Dial("tcp", serverAddr)
+func transferData(src, dst net.Conn) {
+	defer dst.Close()
+	_, err := io.CopyBuffer(dst, src, make([]byte, BufferSize))
 	if err != nil {
-		fmt.Println("Unable to connect to server:", err)
+		log.Printf("Error transferring data: %v\n", err)
+	}
+}
+
+func handleConnection(conn net.Conn, target string) {
+	defer conn.Close()
+
+	// Connect to the target address
+	serverConn, err := net.Dial("tcp", target)
+	if err != nil {
+		log.Printf("Failed to connect to server %s: %v\n", target, err)
 		return
 	}
 	defer serverConn.Close()
 
-	errCh := make(chan error, 1)
+	// Transfer data from the client to the server and vice versa
+	go transferData(conn, serverConn)
+	transferData(serverConn, conn)
+}
 
-	copyData := func(dst io.Writer, src io.Reader) {
-		_, err := io.CopyBuffer(dst, src, make([]byte, BufferSize))
-		errCh <- err
+func startServer(listenAddr string, targetAddr string) {
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatalf("Failed to listen on %s: %v", listenAddr, err)
 	}
+	defer listener.Close()
+	log.Printf("Server listening on %s and forwarding to %s\n", listenAddr, targetAddr)
 
-	go copyData(serverConn, client)
-	go copyData(client, serverConn)
-
-	if err := <-errCh; err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	if err := <-errCh; err != nil {
-		fmt.Println("Error:", err)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v\n", err)
+			continue
+		}
+		go handleConnection(conn, targetAddr)
 	}
 }
 
 func main() {
-	var mode, localPort, destination, serverAddress string
-	flag.StringVar(&mode, "mode", ClientMode, "Mode of operation (client or server)")
-	flag.StringVar(&localPort, "localPort", "", "Local port for client mode")
-	flag.StringVar(&destination, "destination", "", "Destination address for client mode (host:port)")
-	flag.StringVar(&serverAddress, "server-address", "", "Server address for server mode (host:port)")
+	mode := flag.String("mode", ClientMode, "Mode of operation (client or server)")
+	localPort := flag.String("localPort", "8080", "Local port to listen on")
+	destination := flag.String("destination", "", "Destination address (host:port) for client mode")
+	serverAddress := flag.String("server-address", "", "Server address to listen on for server mode")
 	flag.Parse()
 
-	switch mode {
+	switch *mode {
 	case ClientMode:
-		if localPort == "" || destination == "" {
-			fmt.Println("Please provide localPort and destination")
-			return
+		if *destination == "" {
+			log.Fatal("Destination address must be provided in client mode")
 		}
-		localAddr := fmt.Sprintf(":%s", localPort)
-		destinationAddr := destination
-
-		listener, err := net.Listen("tcp", localAddr)
-		if err != nil {
-			fmt.Println("Error listening:", err)
-			return
-		}
-		defer listener.Close()
-		fmt.Println("Listening on", localAddr)
-
-		for {
-			client, err := listener.Accept()
-			if err != nil {
-				fmt.Println("Error accepting connection:", err)
-				return
-			}
-			fmt.Println("Accepted connection from", client.RemoteAddr())
-			go handleClient(client, destinationAddr)
-		}
+		localAddr := fmt.Sprintf(":%s", *localPort)
+		startServer(localAddr, *destination)
 	case ServerMode:
-		if serverAddress == "" {
-			fmt.Println("Please provide server-address")
-			return
+		if *serverAddress == "" {
+			log.Fatal("Server address must be provided in server mode")
 		}
-		serverAddr := serverAddress
-
-		listener, err := net.Listen("tcp", serverAddr)
-		if err != nil {
-			fmt.Println("Error listening:", err)
-			return
-		}
-		defer listener.Close()
-		fmt.Println("Listening on", serverAddr)
-
-		for {
-			client, err := listener.Accept()
-			if err != nil {
-				fmt.Println("Error accepting connection:", err)
-				return
-			}
-			fmt.Println("Accepted connection from", client.RemoteAddr())
-			client.Close()
-		}
+		startServer(*serverAddress, *destination)
 	default:
-		fmt.Println("Invalid mode. Please provide either 'client' or 'server'")
-		return
+		log.Fatalf("Invalid mode: %s", *mode)
 	}
 }
