123fa30b01f698c05f2c5e93af968582eef75628
iamfarhad <farhad.pd@gmail.com>
2024-06-06 23:25:58 +0330

    change

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 var bufferPool = sync.Pool{
 	New: func() interface{} {
-		return make([]byte, 32*1024) // Adjust buffer size as needed
+		return make([]byte, 64*1024) // Increased buffer size for better throughput
 	},
 }
 
@@ -25,7 +25,7 @@ type Forwarder struct {
 }
 
 // Start begins listening on the forwarder's configured port and forwards connections.
-func (f *Forwarder) Start(destinationHost string, bufferSize int, wg *sync.WaitGroup) {
+func (f *Forwarder) Start(destinationHost string, bufferSize int, workerPool chan struct{}, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	listenAddr := fmt.Sprintf(":%d", f.ListenPort)
@@ -45,33 +45,20 @@ func (f *Forwarder) Start(destinationHost string, bufferSize int, wg *sync.WaitG
 			continue
 		}
 
-		tcpConn, ok := conn.(*net.TCPConn)
-		if !ok {
-			log.Printf("Failed to cast to TCPConn: %v", err)
-			continue
+		select {
+		case workerPool <- struct{}{}:
+			go f.handleConnection(conn, targetAddr, bufferSize, workerPool)
+		default:
+			log.Printf("Worker pool full, dropping connection from %s", conn.RemoteAddr().String())
+			conn.Close()
 		}
-
-		if err := tcpConn.SetNoDelay(true); err != nil {
-			log.Printf("Failed to set TCP_NODELAY: %v", err)
-			continue
-		}
-
-		if err := tcpConn.SetWriteBuffer(bufferSize); err != nil {
-			log.Printf("Failed to set write buffer size: %v", err)
-			continue
-		}
-		if err := tcpConn.SetReadBuffer(bufferSize); err != nil {
-			log.Printf("Failed to set read buffer size: %v", err)
-			continue
-		}
-
-		go f.handleConnection(tcpConn, targetAddr, bufferSize)
 	}
 }
 
 // handleConnection forwards a single connection to the destination host and port.
-func (f *Forwarder) handleConnection(src *net.TCPConn, targetAddr string, bufferSize int) {
+func (f *Forwarder) handleConnection(src net.Conn, targetAddr string, bufferSize int, workerPool chan struct{}) {
 	defer src.Close()
+	defer func() { <-workerPool }()
 
 	dst, err := net.Dial("tcp", targetAddr)
 	if err != nil {
@@ -80,44 +67,45 @@ func (f *Forwarder) handleConnection(src *net.TCPConn, targetAddr string, buffer
 	}
 	defer dst.Close()
 
-	dstTcpConn, ok := dst.(*net.TCPConn)
-	if !ok {
-		log.Printf("Failed to cast to TCPConn: %v", err)
+	err = f.copyData(src, dst, bufferSize)
+	if err != nil {
+		log.Printf("Error copying data from %s to %s: %v", src.RemoteAddr().String(), targetAddr, err)
 		return
 	}
-
-	if err := dstTcpConn.SetNoDelay(true); err != nil {
-		log.Printf("Failed to set TCP_NODELAY: %v", err)
-		return
-	}
-
-	if err := dstTcpConn.SetWriteBuffer(bufferSize); err != nil {
-		log.Printf("Failed to set write buffer size: %v", err)
-		return
-	}
-	if err := dstTcpConn.SetReadBuffer(bufferSize); err != nil {
-		log.Printf("Failed to set read buffer size: %v", err)
-		return
-	}
-
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
-	go copyData(src, dstTcpConn, wg, bufferSize)
-	go copyData(dstTcpConn, src, wg, bufferSize)
-	wg.Wait()
 }
 
 // copyData handles the actual data transfer between the source and destination.
-func copyData(src, dst *net.TCPConn, wg *sync.WaitGroup, bufferSize int) {
-	defer wg.Done()
+func (f *Forwarder) copyData(src, dst net.Conn, bufferSize int) error {
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
 
-	buf := bufferPool.Get().([]byte)
-	defer bufferPool.Put(buf)
+	errChan := make(chan error, 2)
 
-	_, err := io.CopyBuffer(dst, src, buf[:bufferSize])
-	if err != nil {
-		log.Printf("Data transfer error: %v", err)
+	go func() {
+		defer wg.Done()
+		buf := bufferPool.Get().([]byte)
+		defer bufferPool.Put(buf)
+		_, err := io.CopyBuffer(dst, src, buf[:bufferSize])
+		errChan <- err
+	}()
+
+	go func() {
+		defer wg.Done()
+		buf := bufferPool.Get().([]byte)
+		defer bufferPool.Put(buf)
+		_, err := io.CopyBuffer(src, dst, buf[:bufferSize])
+		errChan <- err
+	}()
+
+	wg.Wait()
+	close(errChan)
+
+	for err := range errChan {
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func main() {
@@ -125,7 +113,8 @@ func main() {
 
 	listenPorts := flag.String("listen-ports", "21212,21213", "Comma-separated list of ports to listen on")
 	destinationHost := flag.String("destination-host", "localhost", "Destination host to forward to")
-	bufferSize := flag.Int("buffer-size", 32*1024, "Buffer size for TCP connections")
+	bufferSize := flag.Int("buffer-size", 64*1024, "Buffer size for TCP connections")
+	workerCount := flag.Int("workers", 100, "Number of concurrent workers")
 	flag.Parse()
 
 	logFile, err := os.OpenFile("forwarder.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -135,6 +124,8 @@ func main() {
 	defer logFile.Close()
 	log.SetOutput(logFile)
 
+	workerPool := make(chan struct{}, *workerCount)
+
 	ports := strings.Split(*listenPorts, ",")
 	for _, portStr := range ports {
 		port, err := strconv.Atoi(portStr)
@@ -143,7 +134,7 @@ func main() {
 		}
 
 		wg.Add(1)
-		go (&Forwarder{ListenPort: port}).Start(*destinationHost, *bufferSize, &wg)
+		go (&Forwarder{ListenPort: port}).Start(*destinationHost, *bufferSize, workerPool, &wg)
 	}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
