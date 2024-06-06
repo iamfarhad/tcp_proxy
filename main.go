defc92a462f5e6558a21f67283fc6f2a96b33ec7
iamfarhad <farhad.pd@gmail.com>
2024-06-06 21:14:50 +0330

    change

--- main.go
+++ main.go
@@ -11,40 +11,40 @@ import (
 const (
 	ClientMode = "client"
 	ServerMode = "server"
-	BufferSize = 32 * 1024 // 32KB buffer size
+	BufferSize = 128 * 1024 // 32KB buffer size
 )
 
-func transferData(src, dst net.Conn) {
-	defer dst.Close()
-	_, err := io.CopyBuffer(dst, src, make([]byte, BufferSize))
-	if err != nil {
-		log.Printf("Error transferring data: %v\n", err)
+func handleClient(client net.Conn, serverAddr string) {
+	defer client.Close()
+
+	// Establish a connection to the server if a server address is provided
+	if serverAddr != "" {
+		serverConn, err := net.Dial("tcp", serverAddr)
+		if err != nil {
+			log.Printf("Failed to connect to server %s: %v\n", serverAddr, err)
+			return
+		}
+		defer serverConn.Close()
+
+		// Transfer data concurrently in both directions
+		go io.CopyBuffer(serverConn, client, make([]byte, BufferSize))
+		io.CopyBuffer(client, serverConn, make([]byte, BufferSize))
+	} else {
+		log.Println("No destination address provided for server mode, handling local connections only.")
 	}
 }
 
-func handleConnection(conn net.Conn, target string) {
-	defer conn.Close()
-
-	// Connect to the target address
-	serverConn, err := net.Dial("tcp", target)
-	if err != nil {
-		log.Printf("Failed to connect to server %s: %v\n", target, err)
-		return
-	}
-	defer serverConn.Close()
-
-	// Transfer data from the client to the server and vice versa
-	go transferData(conn, serverConn)
-	transferData(serverConn, conn)
-}
-
-func startServer(listenAddr string, targetAddr string) {
+func startServer(listenAddr, targetAddr string) {
 	listener, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		log.Fatalf("Failed to listen on %s: %v", listenAddr, err)
 	}
 	defer listener.Close()
-	log.Printf("Server listening on %s and forwarding to %s\n", listenAddr, targetAddr)
+	log.Printf("Server listening on %s", listenAddr)
+	if targetAddr != "" {
+		log.Printf(" and forwarding to %s", targetAddr)
+	}
+	log.Println()
 
 	for {
 		conn, err := listener.Accept()
@@ -52,30 +52,32 @@ func startServer(listenAddr string, targetAddr string) {
 			log.Printf("Failed to accept connection: %v\n", err)
 			continue
 		}
-		go handleConnection(conn, targetAddr)
+		log.Printf("Accepted connection from %s", conn.RemoteAddr())
+		go handleClient(conn, targetAddr)
 	}
 }
 
 func main() {
-	mode := flag.String("mode", ClientMode, "Mode of operation (client or server)")
-	localPort := flag.String("localPort", "8080", "Local port to listen on")
-	destination := flag.String("destination", "", "Destination address (host:port) for client mode")
-	serverAddress := flag.String("server-address", "", "Server address to listen on for server mode")
+	var mode, localPort, destination, serverAddress string
+	flag.StringVar(&mode, "mode", ClientMode, "Mode of operation (client or server)")
+	flag.StringVar(&localPort, "localPort", "", "Local port to listen on")
+	flag.StringVar(&destination, "destination", "", "Destination address for client mode (host:port)")
+	flag.StringVar(&serverAddress, "server-address", "", "Server address to listen on for server gettingMode")
 	flag.Parse()
 
-	switch *mode {
+	switch mode {
 	case ClientMode:
-		if *destination == "" {
-			log.Fatal("Destination address must be provided in client mode")
+		if localPort == "" || destination == "" {
+			log.Fatal("Please provide localPort and destination for client mode")
 		}
-		localAddr := fmt.Sprintf(":%s", *localPort)
-		startServer(localAddr, *destination)
+		localAddr := fmt.Sprintf(":%s", localPort)
+		startServer(localAddr, destination)
 	case ServerMode:
-		if *serverAddress == "" {
-			log.Fatal("Server address must be provided in server mode")
+		if serverAddress == "" {
+			log.Fatal("Please provide server-address for server mode")
 		}
-		startServer(*serverAddress, *destination)
+		startServer(serverAddress, "")
 	default:
-		log.Fatalf("Invalid mode: %s", *mode)
+		log.Fatalf("Invalid mode: %s. Please specify 'client' or 'server'", mode)
 	}
 }
